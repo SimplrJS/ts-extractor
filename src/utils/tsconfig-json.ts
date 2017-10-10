1c24509c8338937dd85f0bb01efdcb64fb299aad
Martynas Zilinskas <martiogalaltu@gmail.com>
2017-10-10 17:00:09 +0300

    from mz/fs to fs-extra. Added Logger.

--- src/utils/tsconfig-json.ts
+++ src/utils/tsconfig-json.ts
@@ -1,11 +1,10 @@
-import * as fs from "mz/fs";
+import * as fs from "fs-extra";
 import * as path from "path";
 import * as ts from "typescript";
 
 // TODO: Fool proof.
 export async function GetCompilerOptions(fileLocation: string): Promise<ts.CompilerOptions> {
-    const rawContent = await fs.readFile(fileLocation, "utf-8");
-    const json = JSON.parse(rawContent);
+    const json = await fs.readJSON(fileLocation);
 
     const fullPath = path.resolve(fileLocation);
     const dirName = path.dirname(fullPath);
