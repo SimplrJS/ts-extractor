fca32f672feefbcecb895e8b6e98642e2db62720
Dovydas Navickas <dovydas@quatrodev.com>
2017-11-08 06:36:43 +0200

    path-is-inside package removed and local implementation moved utils/into path-is-inside.ts.

--- src/api-helpers.ts
+++ src/api-helpers.ts
@@ -39,6 +39,7 @@ import { ApiConstruct } from "./definitions/api-construct";
 import { ApiTypeParameter } from "./definitions/api-type-parameter";
 import { ApiTypeLiteral } from "./definitions/api-type-literal";
 import { ApiFunctionType } from "./definitions/api-function-type";
+import { PathIsInside } from "./utils/path-is-inside";
 
 export namespace ApiHelpers {
     // TODO: Add return dictionary of ApiItems.
@@ -113,34 +114,6 @@ export namespace ApiHelpers {
         }
 
         return apiItem;
-    }
-
-    export function PathIsInside(thePath: string, potentialParent: string): boolean {
-        // For inside-directory checking, we want to allow trailing slashes, so normalize.
-        thePath = StripTrailingSep(thePath);
-        potentialParent = StripTrailingSep(potentialParent);
-
-        // Node treats only Windows as case-insensitive in its path module; we follow those conventions.
-        if (process.platform === "win32") {
-            thePath = thePath.toLowerCase();
-            potentialParent = potentialParent.toLowerCase();
-        }
-
-        thePath = path.normalize(thePath);
-        potentialParent = path.normalize(potentialParent);
-
-        return thePath.lastIndexOf(potentialParent, 0) === 0 &&
-            (
-                thePath[potentialParent.length] === path.sep ||
-                thePath[potentialParent.length] === undefined
-            );
-    }
-
-    function StripTrailingSep(thePath: string): string {
-        if (thePath[thePath.length - 1] === path.sep) {
-            return thePath.slice(0, -1);
-        }
-        return thePath;
     }
 
     export function ShouldVisit(declaration: ts.Declaration, options: ApiItemOptions): boolean {
