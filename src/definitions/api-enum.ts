cc6bbe1af778f1e7a492f7ee3c7033c2a34d73b6
Martynas Zilinskas <martiogalaltu@gmail.com>
2017-12-08 10:57:26 +0200

    Added IsConst to ApiEnum. Fixes #51.

--- src/definitions/api-enum.ts
+++ src/definitions/api-enum.ts
@@ -10,10 +10,15 @@ import { ApiItemLocationDto } from "../contracts/api-item-location-dto";
 
 export class ApiEnum extends ApiItem<ts.EnumDeclaration, ApiEnumDto> {
     private members: ApiItemReferenceTuple = [];
+    private isConst: boolean;
 
     protected OnGatherData(): void {
+        // IsConst
+        this.isConst = ApiHelpers.ModifierKindExistsInModifiers(this.Declaration.modifiers, ts.SyntaxKind.ConstKeyword);
+
         // Members
         this.members = ApiHelpers.GetItemsIdsFromDeclarations(this.Declaration.members, this.Options);
+
     }
 
     public OnExtract(): ApiEnumDto {
@@ -27,6 +32,7 @@ export class ApiEnum extends ApiItem<ts.EnumDeclaration, ApiEnumDto> {
             KindString: ts.SyntaxKind[this.Declaration.kind],
             Metadata: metadata,
             Location: location,
+            IsConst: this.isConst,
             Members: this.members
         };
     }
