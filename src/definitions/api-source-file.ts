30cddaf010a19d9c02ad9898ad597240c626c5c4
Martynas Žilinskas <martiogalaltu@gmail.com>
2017-12-14 16:24:23 +0200

    Feature: Extracted data update (#54)

--- src/definitions/api-source-file.ts
+++ src/definitions/api-source-file.ts
@@ -8,6 +8,7 @@ import { ApiItemReferenceTuple } from "../contracts/api-item-reference-tuple";
 import { ApiHelpers } from "../api-helpers";
 
 import { ApiMetadataDto } from "../contracts/api-metadata-dto";
+import { ApiItemLocationDto } from "../contracts";
 
 export class ApiSourceFile extends ApiItem<ts.SourceFile, ApiSourceFileDto> {
     private members: ApiItemReferenceTuple;
@@ -16,24 +17,21 @@ export class ApiSourceFile extends ApiItem<ts.SourceFile, ApiSourceFileDto> {
         return path.basename(this.Declaration.fileName);
     }
 
-    private getPath(): string {
-        const relativePath = path.relative(this.Options.ExtractorOptions.ProjectDirectory, this.Declaration.fileName);
-        return ApiHelpers.StandardizeRelativePath(relativePath, this.Options);
-    }
-
     protected OnGatherData(): void {
         this.members = ApiHelpers.GetItemsIdsFromSymbols(this.Symbol.exports, this.Options);
     }
 
     public OnExtract(): ApiSourceFileDto {
         const metadata: ApiMetadataDto = this.GetItemMetadata();
+        const location: ApiItemLocationDto = ApiHelpers.GetApiItemLocationDtoFromDeclaration(this.Declaration, this.Options);
+        const name: string = this.getFileName();
 
         return {
             ApiKind: ApiItemKinds.SourceFile,
-            Name: this.getFileName(),
-            Path: this.getPath(),
+            Name: name,
             Kind: this.Declaration.kind,
             Metadata: metadata,
+            Location: location,
             KindString: ts.SyntaxKind[this.Declaration.kind],
             Members: this.members
         };
