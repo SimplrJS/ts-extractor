8bfe869b45faa713af8749a37723938b3bcc9668
Martynas Žilinskas <martiogalaltu@gmail.com>
2017-12-28 12:59:41 +0200

    Added missing properties to construct dto.

--- src/definitions/api-construct.ts
+++ src/definitions/api-construct.ts
@@ -1,26 +1,14 @@
 import * as ts from "typescript";
-import { ApiItem } from "../abstractions/api-item";
 
 import { ApiHelpers } from "../api-helpers";
 import { ApiConstructDto } from "../contracts/definitions/api-construct-dto";
-import { ApiItemReference } from "../contracts/api-item-reference";
 import { ApiItemKinds } from "../contracts/api-item-kinds";
 
 import { ApiMetadataDto } from "../contracts/api-metadata-dto";
+import { ApiCallableBase } from "../abstractions/api-callable-base";
 import { ApiItemLocationDto } from "../contracts/api-item-location-dto";
 
-export class ApiConstruct extends ApiItem<ts.ConstructSignatureDeclaration, ApiConstructDto> {
-    private isOverloadBase: boolean;
-    private parameters: ApiItemReference[] = [];
-
-    protected OnGatherData(): void {
-        // Overload
-        this.isOverloadBase = this.TypeChecker.isImplementationOfOverload(this.Declaration) || false;
-
-        // Parameters
-        this.parameters = ApiHelpers.GetItemsIdsFromDeclarations(this.Declaration.parameters, this.Options);
-    }
-
+export class ApiConstruct extends ApiCallableBase<ts.ConstructSignatureDeclaration, ApiConstructDto> {
     public OnExtract(): ApiConstructDto {
         const metadata: ApiMetadataDto = this.GetItemMetadata();
         const location: ApiItemLocationDto = ApiHelpers.GetApiItemLocationDtoFromNode(this.Declaration, this.Options);
@@ -32,8 +20,10 @@ export class ApiConstruct extends ApiItem<ts.ConstructSignatureDeclaration, ApiC
             KindString: ts.SyntaxKind[this.Declaration.kind],
             Metadata: metadata,
             Location: location,
-            IsOverloadBase: this.isOverloadBase,
-            Parameters: this.parameters
+            IsOverloadBase: this.IsOverloadBase,
+            Parameters: this.Parameters,
+            ReturnType: this.ReturnType,
+            TypeParameters: this.TypeParameters
         };
     }
 }
