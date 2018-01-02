05cdc3cc776f8cd0ebd23179885c7456592b837f
Martynas Žilinskas <martiogalaltu@gmail.com>
2018-01-02 13:22:44 +0200

    Added get/set accessors.

--- src/contracts/definitions/api-get-accessor-dto.ts
+++ src/contracts/definitions/api-get-accessor-dto.ts
@@ -1,6 +1,8 @@
 import { ApiBaseItemDto } from "../api-base-item-dto";
 import { ApiItemKinds } from "../api-item-kinds";
+import { TypeDto } from "../type-dto";
 
 export interface ApiGetAccessorDto extends ApiBaseItemDto {
     ApiKind: ApiItemKinds.GetAccessor;
+    Type: TypeDto;
 }
--- src/contracts/definitions/api-set-accessor-dto.ts
+++ src/contracts/definitions/api-set-accessor-dto.ts
@@ -1,6 +1,8 @@
 import { ApiBaseItemDto } from "../api-base-item-dto";
 import { ApiItemKinds } from "../api-item-kinds";
+import { ApiItemReference } from "../api-item-reference";
 
 export interface ApiSetAccessorDto extends ApiBaseItemDto {
     ApiKind: ApiItemKinds.SetAccessor;
+    Parameter: ApiItemReference | undefined;
 }
