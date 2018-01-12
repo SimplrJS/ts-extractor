c009238de2b7eb7cc8a7976de8c013c3ffa47927
Martynas Žilinskas <martiogalaltu@gmail.com>
2018-01-12 10:45:29 +0200

    Updated definitions.

--- src/contracts/definitions/api-interface-dto.ts
+++ src/contracts/definitions/api-interface-dto.ts
@@ -1,11 +1,11 @@
 import { ApiBaseItemDto } from "../api-base-item-dto";
 import { ApiItemReference } from "../api-item-reference";
-import { TypeDto } from "../type-dto";
+import { ApiType } from "../api-type";
 import { ApiItemKinds } from "../api-item-kinds";
 
 export interface ApiInterfaceDto extends ApiBaseItemDto {
     ApiKind: ApiItemKinds.Interface;
     TypeParameters: ApiItemReference[];
     Members: ApiItemReference[];
-    Extends: TypeDto[];
+    Extends: ApiType[];
 }
