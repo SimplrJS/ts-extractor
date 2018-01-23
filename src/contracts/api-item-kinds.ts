5c0b2c7c12f546e3141c3696f40d01eea1e5b89e
Martynas Žilinskas <martiogalaltu@gmail.com>
2018-01-23 17:54:20 +0200

    Added ImportNamespace as ApiItemKind.

--- src/contracts/api-item-kinds.ts
+++ src/contracts/api-item-kinds.ts
@@ -6,6 +6,7 @@ export enum ApiItemKinds {
     Interface = "interface",
     Method = "method",
     Namespace = "namespace",
+    ImportNamespace = "import-namespace",
     Parameter = "parameter",
     Property = "property",
     Variable = "variable",
