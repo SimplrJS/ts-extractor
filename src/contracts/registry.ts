6e4a82e8efbdf58435324349a374579c4adb42e3
Martynas Žilinskas <martiogalaltu@gmail.com>
2018-01-26 12:39:53 +0200

    Removed obvious comments.

--- src/contracts/registry.ts
+++ src/contracts/registry.ts
@@ -1,15 +1,10 @@
 import * as ts from "typescript";
 
 export interface ReadonlyRegistry<TItem> {
-    // Extraction
     Extract(): void;
-
-    // Existence checks
     HasDeclaration(declaration: ts.Declaration | undefined): boolean;
-
     GetDeclarationId(declaration: ts.Declaration | undefined): string | undefined;
     Get(id: string): TItem | undefined;
-
     Registry: ReadonlyMap<string, TItem>;
 }
 
