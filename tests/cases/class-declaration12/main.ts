1a765f993222e906662b41ae4c4402763191e848
Martynas Žilinskas <martiogalaltu@gmail.com>
2018-02-01 19:00:11 +0200

    Added test cases.

---
+++ tests/cases/class-declaration12/main.ts
@@ -0,0 +1,10 @@
+// Simple class.
+
+/**
+ * Class Foo comment line.
+ * @summary Summary of comment line.
+ * @template TProps component props
+ * @template TState component state
+ */
+export class FooComponent<TProps, TState> {
+}
