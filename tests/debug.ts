c347965af6c32ddc559d22ce481d4daa06c8c9be
Martynas Žilinskas <martiogalaltu@gmail.com>
2018-03-16 19:39:54 +0200

    Debug file commented.

--- tests/debug.ts
+++ tests/debug.ts
@@ -1,40 +1,40 @@
-import * as path from "path";
-import { GetCompilerOptions } from "./utils/tsconfig-json";
-import * as fs from "fs-extra";
+// import * as path from "path";
+// import { GetCompilerOptions } from "./utils/tsconfig-json";
+// import * as fs from "fs-extra";
 
-import { Extractor } from "./extractor";
-import { ApiHelpers } from "./api-helpers";
-import { AccessModifier } from "./contracts";
+// import { Extractor } from "./extractor";
+// import { ApiHelpers } from "./api-helpers";
+// import { AccessModifier } from "./contracts";
 
-async function main(): Promise<void> {
-    const projectDirectory = path.resolve(__dirname, "./examples/simple/");
-    const tsconfigPath = path.join(projectDirectory, "tsconfig.json");
-    const compilerOptions = await GetCompilerOptions(tsconfigPath);
+// async function main(): Promise<void> {
+//     const projectDirectory = path.resolve(__dirname, "./examples/simple/");
+//     const tsconfigPath = path.join(projectDirectory, "tsconfig.json");
+//     const compilerOptions = await GetCompilerOptions(tsconfigPath);
 
-    const extractor = new Extractor({
-        CompilerOptions: compilerOptions,
-        ProjectDirectory: projectDirectory,
-        ExternalPackages: ["typescript"],
-        FilterApiItems: apiItem => {
-            const accessModifier = ApiHelpers.ResolveAccessModifierFromModifiers(apiItem.Declaration.modifiers);
-            if (accessModifier === AccessModifier.Private) {
-                return false;
-            }
+//     const extractor = new Extractor({
+//         CompilerOptions: compilerOptions,
+//         ProjectDirectory: projectDirectory,
+//         ExternalPackages: ["typescript"],
+//         FilterApiItems: apiItem => {
+//             const accessModifier = ApiHelpers.ResolveAccessModifierFromModifiers(apiItem.Declaration.modifiers);
+//             if (accessModifier === AccessModifier.Private) {
+//                 return false;
+//             }
 
-            const metadata = apiItem.GetItemMetadata();
-            if (metadata.JSDocTags.findIndex(x => x.name === "private") !== -1) {
-                return false;
-            }
+//             const metadata = apiItem.GetItemMetadata();
+//             if (metadata.JSDocTags.findIndex(x => x.name === "private") !== -1) {
+//                 return false;
+//             }
 
-            return true;
-        }
-    });
+//             return true;
+//         }
+//     });
 
-    const extract1 = extractor.Extract([path.resolve("examples/simple/index.ts")]);
-    // tslint:disable-next-line:no-console
-    console.log(JSON.stringify(extract1, null, 4));
+//     const extract1 = extractor.Extract([path.resolve("examples/simple/index.ts")]);
+//     // tslint:disable-next-line:no-console
+//     console.log(JSON.stringify(extract1, null, 4));
 
-    await fs.writeJson("./debug.json", extract1);
-}
+//     await fs.writeJson("./debug.json", extract1);
+// }
 
-main();
+// main();
