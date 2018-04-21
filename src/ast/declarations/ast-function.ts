56250540f03f6ee13949d0ef3392b3fac5302e7f
Martynas Žilinskas <martiogalaltu@gmail.com>
2018-04-21 11:32:02 +0300

    Added return type for AstFunction.

--- src/ast/declarations/ast-function.ts
+++ src/ast/declarations/ast-function.ts
@@ -3,6 +3,7 @@ import { AstDeclarationBase } from "../ast-declaration-base";
 import { AstItemMemberReference, AstItemKind } from "../../contracts/ast-item";
 import { AstSymbol } from "../ast-symbol";
 import { AstItemGatherMembersOptions } from "../../abstractions/ast-item-base";
+import { AstTypeBase } from "../ast-type-base";
 // import { TsHelpers } from "../../ts-helpers";
 
 export class AstFunction extends AstDeclarationBase<ts.FunctionDeclaration, {}> {
@@ -24,9 +25,34 @@ export class AstFunction extends AstDeclarationBase<ts.FunctionDeclaration, {}> 
         return this.parametersReferences.map(x => this.options.itemsRegistry.get(x.id)) as AstSymbol[];
     }
 
+    public get returnType(): AstTypeBase | undefined {
+        if (this.returnTypeReference == null) {
+            return undefined;
+        }
+
+        return this.options.itemsRegistry.get(this.returnTypeReference.id) as AstTypeBase;
+    }
+
     private parametersReferences: AstItemMemberReference[] = [];
 
+    private returnTypeReference: AstItemMemberReference | undefined;
+
     protected onGatherMembers(options: AstItemGatherMembersOptions): AstItemMemberReference[] {
-        return [];
+        const members: AstItemMemberReference[] = [];
+
+        // Resolved return Type.
+        const signature = this.typeChecker.getSignatureFromDeclaration(this.item);
+        if (signature != null) {
+            const type = this.typeChecker.getReturnTypeOfSignature(signature);
+
+            const returnAstType = this.options.resolveAstType(type, undefined, { parentId: this.getId() });
+            if (!this.options.itemsRegistry.hasItem(type)) {
+                options.addAstItemToRegistry(returnAstType);
+            }
+            this.returnTypeReference = { id: returnAstType.getId() };
+            members.push(this.returnTypeReference);
+        }
+
+        return members;
     }
 }
